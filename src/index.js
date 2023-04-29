10d29f91ade9b07b4cbec469a576a48b27d5f1d7
Kevin Hoang <kevin.hoang199@gmail.com>
2023-04-28 20:49:36 -0400

    Completed requirements with some styling.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
 import App from "./App";
+import Header from "./Header"
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
+    <Header />
     <App />
+    
   </StrictMode>
 );
